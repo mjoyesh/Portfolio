ab647d2341f457f58d13e104b49c901926aa21ec
Joyesh <joyesh.malik@tothenew.com>
2019-04-28 18:24:58 +0530

    Updates

---
+++ precache-manifest.c8d49ec6db38782484d22c1d63a07e19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdab0d0511dcd28e7529c78065b2e87",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "02d34719e6f5a001e170",
+    "url": "/Portfolio/static/css/main.c53ece66.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/Portfolio/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "02d34719e6f5a001e170",
+    "url": "/Portfolio/static/js/main.e350e3ee.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  }
+]);
