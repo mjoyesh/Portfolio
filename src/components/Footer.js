0e228bed95a8248b40615632fe2169ecdd472159
Joyesh <joyesh.malik@tothenew.com>
2019-04-28 18:04:55 +0530

    Portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,30 @@
 import React, {Component} from 'react';
-import { HashRouter, Link } from 'react-router-dom'
+// import { HashRouter, Link } from 'react-router-dom'
 import './style.css';
 class Footer extends Component{
     render(){
         return (
           <footer>
-              <HashRouter>
+              {/* <HashRouter> */}
                 <div className="container">
                     <p>CONTACT ME HERE !</p>
-                    <ul className="social-icons">
+                    {/* <ul className="social-icons">
                         <li><Link to="https://github.com/mjoyesh"><i class="fab fa-github"></i></Link></li>
                         <li><Link to="https://github.com/mjoyesh"><i class="fab fa-linkedin"></i></Link></li>
                         <li><Link to="https://github.com/mjoyesh"><i class="fab fa-facebook"></i></Link></li>
                         <li><Link to="https://github.com/mjoyesh"><i class="fab fa-instagram"></i></Link></li>
                         <li><Link to="https://github.com/mjoyesh"><i class="fab fa-twitter"></i></Link></li>
+                    </ul> */}
+
+                    <ul className="social-icons">
+                        <li><a href="https://github.com/mjoyesh"><i class="fab fa-github"></i></a></li>
+                        <li><a href="https://github.com/mjoyesh"><i class="fab fa-linkedin"></i></a></li>
+                        <li><a href="https://github.com/mjoyesh"><i class="fab fa-facebook"></i></a></li>
+                        <li><a href="https://github.com/mjoyesh"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="https://github.com/mjoyesh"><i class="fab fa-twitter"></i></a></li>
                     </ul>
                 </div>
-              </HashRouter>
+              {/* </HashRouter> */}
           </footer>
           );
     }
