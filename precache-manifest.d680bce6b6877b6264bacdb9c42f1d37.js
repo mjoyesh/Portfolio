31eea91d9b501a3d3a83ab2155af94fd5f2b0a40
Joyesh <joyesh.malik@tothenew.com>
2019-04-28 17:51:06 +0530

    Updates

---
+++ precache-manifest.d680bce6b6877b6264bacdb9c42f1d37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7907c0b6ef8bae69487821751fd7be79",
+    "url": "/Portfolio/index.html/index.html"
+  },
+  {
+    "revision": "2e8cc83686c8c9fda51a",
+    "url": "/Portfolio/index.html/static/css/main.c53ece66.chunk.css"
+  },
+  {
+    "revision": "1081205354213076c389",
+    "url": "/Portfolio/index.html/static/js/2.065f0129.chunk.js"
+  },
+  {
+    "revision": "2e8cc83686c8c9fda51a",
+    "url": "/Portfolio/index.html/static/js/main.433d462a.chunk.js"
+  },
+  {
+    "revision": "2a97a263088dedf221d4",
+    "url": "/Portfolio/index.html/static/js/runtime~main.01cb472d.js"
+  }
+]);
